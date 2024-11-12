3344f48c2c63219582216b8c61da53bfba1b31aa
Sandip kumar <sandipsingh2254@gmail.com>
2024-11-12 12:20:49 +0530

    12 nov js code demo

---
+++ 12 nov/script.js
@@ -0,0 +1,16 @@
+// variables
+// var ,let ,const (three way to define variables)
+
+var name ="sandip rajput";
+console.log (typeof name);
+var age = 24;
+var isEligible= true;
+var data =null;
+var firstName;
+
+
+// rules for writing variable name 
+// 1. should not start with a number or underscore
+// 2. can be written in pascal style or camel style
+// 3. underscore or dollar signs should not
+//  be 
