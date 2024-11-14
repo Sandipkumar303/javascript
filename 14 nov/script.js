76f1a3a1ea109880c8ea89cbd76650b1e06534be
Sandip kumar <sandipsingh2254@gmail.com>
2024-11-14 12:32:56 +0530

    14 nov code add style using js

---
+++ 14 nov/script.js
@@ -0,0 +1,41 @@
+//variable
+// var firstname ="john";
+// {
+//     let lastname="doe";
+//     const age= 35;
+//     console.log (lastname, age)
+
+// }
+
+// let num1= 6565654545n;
+// console.log(typeof num1);
+
+//  console.log("hello");
+//  document.write("hello javascript");
+//  alert("welcome");
+
+// document.getElementById("demo").innerHTML = "Hello world";
+// document.getElementById("demo").innerHTML="welcome to javascript";
+
+// changing the inner html -->
+var h1=document.getElementById("demo");
+h1.innerHTML="hello world";
+
+// changing the css of element --> 
+h1.style.color= "red";
+h1.style.backgroundColor= "yellow";
+h1.style.paddingTop="300px";
+h1.style.border="5px solid black";
+h1.style.fontSize="200px";
+h1.style.height="100%";
+var button=document.getElementById("btn");
+button.innerHTML="Show";
+button.style.background="black";
+button.style.color="white";
+button.style.border="solid";
+button.style.borderRadius="20px";
+button.style.fontSize="100px";
+
+
+// make a h1 ,p and button and add innerhtml to it 
+
