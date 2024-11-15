3480e29b2ab1f683e80d0b2f1dbc8ce212b300a0
Sandip kumar <sandipsingh2254@gmail.com>
2024-11-15 12:33:49 +0530

    15 nov add style

---
+++ 15 nov/script.js
@@ -0,0 +1,40 @@
+//padding , border , fontsize, height, apply
+// h1.style.padding="5px";
+// h1.style.border=" 5px solid black";
+// h1.style.fontSize="50px";
+// h1.style.height="80px";
+
+
+// var btn=document.getElementById("btn");
+// btn.style.width="150px";
+// btn.style.height="50px";
+// btn.innerHTML="Show";
+// btn.style.borderRadius="8px";
+// btn.style.fontFamily="Arial";
+// btn.style.fontSize="20px";
+// btn.style.backgroundColor="gray";
+// btn.style.boxShadow="10px 20px 30px gray";
+var h1=document.getElementById("demo");
+// h1.innerHTML=" Do You Work Everyday";
+
+var name="John Doe";
+var age=34;
+// concatination
+console.log(name + " " + age);
+h1.innerHTML=name + " " + age;
+
+// to get value from ser
+
+// var userName= prompt("Enter Your Name");
+// var userAge= prompt("Enter Your Age");
+
+// h1.innerHTML=userNmae+ " "+ userAge;
+//  var txtcolor=prompt(" Enter your color");
+// h1.style.color=txtcolor;
+// var bgcolor=prompt(" Enter your Bac kgraound color");
+// h1.style.backgroundColor=bgcolor;
+
+// get 
+
+ var  Myimage= document.getElementById("Myimage");
+ Myimage.src="1.img" ;
